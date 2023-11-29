274c2f9950b7adfcea3ed1cb6142e5d9745abe55
Troxsoft <troxz6@outlook.com>
2023-11-29 17:37:51 -0500

    bugs arreglados + ejemplo + ejemplo  compilado

--- engine/physics.go
+++ engine/physics.go
@@ -2,24 +2,30 @@ package engine
 
 import "fmt"
 
-func _newGameObjectUnregister(pos Position, siz Size) *GameObject {
-	return &GameObject{
-		name: fmt.Sprintf("%s_'un_register_______________________________'", len(gameObjects)),
+func _newGameObjectUnregister(pos Position, siz Size, shape Shape, id int) *GameObject {
+	j := &GameObject{
+		name:     fmt.Sprintf("%s_'un_register_______________________________'", len(gameObjects)),
+		size:     siz,
+		position: pos,
+		shape:    shape,
+		id:       id,
 	}
+	j.collision = NewCollisionRectagle(j)
+	return j
 }
 
 /*
 physics type: Colliders and collisions[ON]
 */
-func (g *GameObject) MoveTo(npos Position) bool {
+func (g *GameObject) MoveTo(npos Position) (*GameObject, bool) {
 
-	coll2 := _newGameObjectUnregister(npos, NewSize(g.size.W, g.size.H))
+	coll2 := _newGameObjectUnregister(npos, NewSize(g.size.W, g.size.H), g.shape, g.id)
 	if h, _ := coll2.collision.OnCollision(); h != nil {
 
-		return false
+		return h, false
 	} else {
 		g.SetPosition(npos)
-		return true
+		return nil, true
 	}
 
 }
---
+++ examples/example_physics_simply.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	e "github.com/Troxsoft/Furia2D-Engine/engine"
+)
+
+var obj *e.GameObject
+var obj2 *e.GameObject
+
+func main() {
+	e.InitGame("welcome to Furia2D-Engine :)", e.NewSize(600, 600), func(ge *e.GameEvent) {
+		obj, _ = e.CreateGameObject("you", e.SHAPE_RECTANGLE, e.NewSize(30, 30), e.NewPosition(200, 30))
+		obj2, _ = e.CreateGameObject("you not is enemy", e.SHAPE_RECTANGLE, e.NewSize(50, 50), e.NewPosition(300, 30))
+		obj.SetUpdate(func(g *e.GameObject, goe *e.GameObjectEvent) {
+			g.SetPosition(e.ClampPos(g.Position(), e.NewPosition(600, 600), e.NewPosition(0, 0)))
+			if ge.IsKeyDown(ge.KeyRight) {
+				objetoColision, _ := g.MoveTo(e.NewPosition(g.Position().X+10, g.Position().Y))
+				if objetoColision != nil {
+					g.SetColor3(255, 0, 0)
+				} else {
+					g.SetColor3(0, 0, 0)
+				}
+			}
+			if ge.IsKeyDown(ge.KeyLeft) {
+				g.MoveTo(e.NewPosition(g.Position().X-10, g.Position().Y))
+			}
+
+		})
+		obj.Instance(nil)
+		obj2.Instance(nil)
+		//fmt.Println(obj.F)
+	},
+		func(ge *e.GameEvent) {
+
+		})
+}
